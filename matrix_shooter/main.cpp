d2f42b172b9382738b917efbdccc5d9163e3da7a
ninad <ninad2eyes@gmail.com>
2024-11-14 19:06:11 -0500

    okay finally start

--- matrix_shooter/main.cpp
+++ matrix_shooter/main.cpp
@@ -1,10 +1,11 @@
 #define GL_SILENCE_DEPRECATION
+#define STB_IMAGE_IMPLEMENTATION
+#define LOG(argument) std::cout << argument << '\n'
 #define GL_GLEXT_PROTOTYPES 1
 #define FIXED_TIMESTEP 0.0166666f
+#define ENEMY_COUNT 1
 #define LEVEL1_WIDTH 14
-#define LEVEL1_HEIGHT 8
-#define LEVEL1_LEFT_EDGE 5.0f
-#define MAX_RGB 255
+#define LEVEL1_HEIGHT 5
 
 #ifdef _WINDOWS
 #include <GL/glew.h>
@@ -16,23 +17,34 @@
 #include "glm/mat4x4.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "ShaderProgram.h"
+#include "stb_image.h"
 #include "cmath"
 #include <ctime>
 #include <vector>
 #include "Entity.h"
 #include "Map.h"
-#include "Utility.h"
-#include "Scene.h"
-#include "LevelA.h"
-#include "Effects.h"
 
-// ––––– CONSTANTS ––––– //
-constexpr int WINDOW_WIDTH = 1280,
-WINDOW_HEIGHT = 960;
+// ————— GAME STATE ————— //
+struct GameState
+{
+    Entity* player;
+    Entity* enemies;
 
-constexpr float BG_RED = 27.0f / MAX_RGB,
-BG_BLUE = 43.0f / MAX_RGB,
-BG_GREEN = 52.0f / MAX_RGB,
+    Map* map;
+
+    Mix_Music* bgm;
+    Mix_Chunk* jump_sfx;
+};
+
+enum AppStatus { RUNNING, TERMINATED };
+
+// ————— CONSTANTS ————— //
+constexpr int WINDOW_WIDTH = 640 * 2,
+WINDOW_HEIGHT = 480 * 2;
+
+constexpr float BG_RED = 0.1922f,
+BG_BLUE = 0.549f,
+BG_GREEN = 0.9059f,
 BG_OPACITY = 1.0f;
 
 constexpr int VIEWPORT_X = 0,
@@ -40,31 +52,42 @@ VIEWPORT_Y = 0,
 VIEWPORT_WIDTH = WINDOW_WIDTH,
 VIEWPORT_HEIGHT = WINDOW_HEIGHT;
 
+constexpr char GAME_WINDOW_NAME[] = "Hello, Maps!";
+
 constexpr char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
 F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
 
 constexpr float MILLISECONDS_IN_SECOND = 1000.0;
 
-enum AppStatus { RUNNING, TERMINATED };
+constexpr char SPRITESHEET_FILEPATH[] = "assets/images/george_0.png",
+MAP_TILESET_FILEPATH[] = "assets/images/tileset.png",
+BGM_FILEPATH[] = "assets/audio/dooblydoo.mp3",
+JUMP_SFX_FILEPATH[] = "assets/audio/bounce.wav";
 
-// ––––– GLOBAL VARIABLES ––––– //
-Scene* g_curr_scene = nullptr;
-LevelA* g_levelA = nullptr;
+constexpr int NUMBER_OF_TEXTURES = 1;
+constexpr GLint LEVEL_OF_DETAIL = 0;
+constexpr GLint TEXTURE_BORDER = 0;
 
-Effects* g_effects = nullptr;
-Scene* g_levels[1];
+unsigned int LEVEL_1_DATA[] =
+{
+    0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+    2, 2, 1, 1, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2
+};
 
-SDL_Window* g_display_window = nullptr;
+// ————— VARIABLES ————— //
+GameState g_game_state;
 
+SDL_Window* g_display_window;
+AppStatus g_app_status = RUNNING;
 ShaderProgram g_shader_program;
 glm::mat4 g_view_matrix, g_projection_matrix;
 
-float g_previous_ticks = 0.0f;
-float g_accumulator = 0.0f;
+float g_previous_ticks = 0.0f,
+g_accumulator = 0.0f;
 
-bool g_is_colliding_bottom = false;
-
-AppStatus g_app_status = RUNNING;
 
 void initialise();
 void process_input();
@@ -72,33 +95,61 @@ void update();
 void render();
 void shutdown();
 
-void switch_to_scene(Scene* scene)
+// ————— GENERAL FUNCTIONS ————— //
+GLuint load_texture(const char* filepath)
 {
-    g_curr_scene = scene;
-    g_curr_scene->initialise();
+    int width, height, number_of_components;
+    unsigned char* image = stbi_load(filepath, &width, &height, &number_of_components, STBI_rgb_alpha);
+
+    if (image == NULL)
+    {
+        LOG("Unable to load image. Make sure the path is correct.");
+        assert(false);
+    }
+
+    GLuint texture_id;
+    glGenTextures(NUMBER_OF_TEXTURES, &texture_id);
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+    glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    stbi_image_free(image);
+
+    return texture_id;
 }
 
 void initialise()
 {
+    // ————— GENERAL ————— //
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
-    g_display_window = SDL_CreateWindow("Hello, Special Effects!",
+    g_display_window = SDL_CreateWindow(GAME_WINDOW_NAME,
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         WINDOW_WIDTH, WINDOW_HEIGHT,
         SDL_WINDOW_OPENGL);
 
     SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
     SDL_GL_MakeCurrent(g_display_window, context);
+    if (context == nullptr)
+    {
+        LOG("ERROR: Could not create OpenGL context.\n");
+        shutdown();
+    }
 
 #ifdef _WINDOWS
     glewInit();
 #endif
 
+    // ————— VIDEO SETUP ————— //
     glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
 
     g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
 
     g_view_matrix = glm::mat4(1.0f);
-    g_projection_matrix = glm::ortho(-3.75f, 3.75f, -2.8125f, 2.8125f, -1.0f, 1.0f);
+    g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
 
     g_shader_program.set_projection_matrix(g_projection_matrix);
     g_shader_program.set_view_matrix(g_view_matrix);
@@ -107,21 +158,60 @@ void initialise()
 
     glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
 
+    // ————— MAP SET-UP ————— //
+    GLuint map_texture_id = load_texture(MAP_TILESET_FILEPATH);
+    g_game_state.map = new Map(LEVEL1_WIDTH, LEVEL1_HEIGHT, LEVEL_1_DATA, map_texture_id, 1.0f, 4, 1);
+
+    // ————— GEORGE SET-UP ————— //
+
+    GLuint player_texture_id = load_texture(SPRITESHEET_FILEPATH);
+
+    int player_walking_animation[4][4] =
+    {
+        { 1, 5, 9, 13 },  // for George to move to the left,
+        { 3, 7, 11, 15 }, // for George to move to the right,
+        { 2, 6, 10, 14 }, // for George to move upwards,
+        { 0, 4, 8, 12 }   // for George to move downwards
+    };
+
+    glm::vec3 acceleration = glm::vec3(0.0f, -4.905f, 0.0f);
+
+    g_game_state.player = new Entity(
+        player_texture_id,         // texture id
+        5.0f,                      // speed
+        acceleration,              // acceleration
+        3.0f,                      // jumping power
+        player_walking_animation,  // animation index sets
+        0.0f,                      // animation time
+        4,                         // animation frame amount
+        0,                         // current animation index
+        4,                         // animation column amount
+        4,                         // animation row amount
+        0.9f,                      // width
+        0.9f,                       // height
+        PLAYER
+    );
+
+
+    // Jumping
+    g_game_state.player->set_jumping_power(5.0f);
+
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096);
+
+    g_game_state.bgm = Mix_LoadMUS(BGM_FILEPATH);
+    //    Mix_PlayMusic(g_game_state.bgm, -1);
+    //    Mix_VolumeMusic(MIX_MAX_VOLUME / 16.0f);
+
+    g_game_state.jump_sfx = Mix_LoadWAV(JUMP_SFX_FILEPATH);
+
+    // ————— BLENDING ————— //
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    g_levelA = new LevelA();
-    g_levels[0] = g_levelA;
-
-    switch_to_scene(g_levels[0]);
-
-    g_effects = new Effects(g_projection_matrix, g_view_matrix);
 }
 
 void process_input()
 {
-    // VERY IMPORTANT: If nothing is pressed, we don't want to go anywhere
-    g_curr_scene->get_state().player->set_movement(glm::vec3(0.0f));
+    g_game_state.player->set_movement(glm::vec3(0.0f));
 
     SDL_Event event;
     while (SDL_PollEvent(&event))
@@ -134,9 +224,19 @@ void process_input()
 
         case SDL_KEYDOWN:
             switch (event.key.keysym.sym) {
-            case SDLK_q: g_app_status = TERMINATED;     break;
+            case SDLK_q:
+                // Quit the game with a keystroke
+                g_app_status = TERMINATED;
+                break;
 
-                //                    case SDLK_e: g_effects->start(SHAKE, 1.0f); break;
+            case SDLK_SPACE:
+                // Jump
+                if (g_game_state.player->get_collided_bottom())
+                {
+                    g_game_state.player->jump();
+                    Mix_PlayChannel(-1, g_game_state.jump_sfx, 0);
+                }
+                break;
 
             default:
                 break;
@@ -147,22 +247,13 @@ void process_input()
         }
     }
 
-    const Uint8* key_state = SDL_GetKeyboardState(nullptr);
+    const Uint8* key_state = SDL_GetKeyboardState(NULL);
 
-    if (key_state[SDL_SCANCODE_C])
-    {
-        g_curr_scene->get_state().player->set_player_state(CHARGING);
-        g_effects->start(SHAKE);
-    }
-    else
-    {
-        g_curr_scene->get_state().player->set_player_state(REST);
-        g_effects->start(NONE);
-    }
+    if (key_state[SDL_SCANCODE_LEFT])       g_game_state.player->move_left();
+    else if (key_state[SDL_SCANCODE_RIGHT]) g_game_state.player->move_right();
 
-    if (glm::length(g_curr_scene->get_state().player->get_movement()) > 1.0f)
-        g_curr_scene->get_state().player->normalise_movement();
-
+    if (glm::length(g_game_state.player->get_movement()) > 1.0f)
+        g_game_state.player->normalise_movement();
 }
 
 void update()
@@ -179,32 +270,19 @@ void update()
         return;
     }
 
-    while (delta_time >= FIXED_TIMESTEP) {
-        g_curr_scene->update(FIXED_TIMESTEP);
-        g_effects->update(FIXED_TIMESTEP);
-
-        g_is_colliding_bottom = g_curr_scene->get_state().player->get_collided_bottom();
-
+    while (delta_time >= FIXED_TIMESTEP)
+    {
+        g_game_state.player->update(FIXED_TIMESTEP, g_game_state.player, NULL, 0,
+            g_game_state.map);
         delta_time -= FIXED_TIMESTEP;
     }
 
     g_accumulator = delta_time;
 
-    // Prevent the camera from showing anything outside of the "edge" of the level
     g_view_matrix = glm::mat4(1.0f);
 
-    if (g_curr_scene->get_state().player->get_position().x > LEVEL1_LEFT_EDGE) {
-        g_view_matrix = glm::translate(g_view_matrix,
-            glm::vec3(-g_curr_scene->get_state().player->get_position().x,
-                3.75,
-                0)
-        );
-    }
-    else {
-        g_view_matrix = glm::translate(g_view_matrix, glm::vec3(-5, 3.75, 0));
-    }
-
-    g_view_matrix = glm::translate(g_view_matrix, g_effects->get_view_offset());
+    // Camera Follows the player
+    g_view_matrix = glm::translate(g_view_matrix, glm::vec3(-g_game_state.player->get_position().x, 0.0f, 0.0f));
 }
 
 void render()
@@ -213,10 +291,8 @@ void render()
 
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // ————— RENDERING THE SCENE (i.e. map, character, enemies...) ————— //
-    g_curr_scene->render(&g_shader_program);
-
-    g_effects->render();
+    g_game_state.player->render(&g_shader_program);
+    g_game_state.map->render(&g_shader_program);
 
     SDL_GL_SwapWindow(g_display_window);
 }
@@ -225,11 +301,14 @@ void shutdown()
 {
     SDL_Quit();
 
-    delete g_levelA;
-    delete g_effects;
+    delete[] g_game_state.enemies;
+    delete    g_game_state.player;
+    delete    g_game_state.map;
+    Mix_FreeChunk(g_game_state.jump_sfx);
+    Mix_FreeMusic(g_game_state.bgm);
 }
 
-// ––––– DRIVER GAME LOOP ––––– //
+// ————— GAME LOOP ————— //
 int main(int argc, char* argv[])
 {
     initialise();
@@ -238,10 +317,6 @@ int main(int argc, char* argv[])
     {
         process_input();
         update();
-
-        //        if (g_curr_scene->get_state().next_scene_id >= 0)
-        //            switch_to_scene(g_levels[g_curr_scene->get_state().next_scene_id]);
-
         render();
     }
 
