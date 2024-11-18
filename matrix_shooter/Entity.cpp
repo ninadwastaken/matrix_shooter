9404a91fad774f4674105ae371f7819ffe4d94b7
ninad <ninad2eyes@gmail.com>
2024-11-18 03:34:06 -0500

    added headers

--- matrix_shooter/Entity.cpp
+++ matrix_shooter/Entity.cpp
@@ -1,3 +1,14 @@
+/**
+* Author: Ninad Moharir
+* Assignment: Rise of the AI
+* Date due: 2024-11-03, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 
